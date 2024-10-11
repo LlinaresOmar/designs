9a2411f25af4efc6c725e1fbab26c475a7571e38
Omar Llinares <llinares.omar@gmail.com>
2024-10-11 09:48:20 +0200

    Pagina26

--- lib/screens/scroll_design.dart
+++ lib/screens/scroll_design.dart
@@ -7,9 +7,69 @@ class ScrollDesign extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+      body: PageView(children: [Page1(), Page1()])
+    );
+  }
+}
+
+class Page1 extends StatelessWidget {
+  const Page1({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Background(),
+        MainContent()
+    
+      ],
+    );
+  }
+}
+
+class MainContent extends StatelessWidget {
+  const MainContent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var textStyle = TextStyle(
+      fontSize: 50,
+      fontWeight: FontWeight.bold,
+      color: Colors.white
+    );
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 10),
+          Text('20º', style: textStyle),
+          Text('Viernes', style: textStyle),
+          Expanded(child: Container()),
+          Icon(
+            Icons.keyboard_arrow_down,
+             size: 100,
+            color: Colors.white
+          )
+        ],
       ),
+    );
+  }
+}
+
+class Background extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color:  Color(0xff30BAD6),
+      alignment: Alignment.topCenter,
+      child: Image(image: AssetImage('assets/scroll-1.png'))
     );
   }
 }
