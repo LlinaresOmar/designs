5504f1fb5895d77c90f2a53a2644ab4361428a78
Omar Llinares <llinares.omar@gmail.com>
2024-10-11 08:59:55 +0200

    Subida de archivos

---
+++ lib/widgets/basic_design.dart
@@ -0,0 +1,114 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+class BasicDesignScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column( 
+        children: [
+          Image(
+            image: AssetImage('assets/landscape.png'),
+          ),
+          // Title
+          Title(),
+
+          // Button Selection
+          ButtonSection(),
+
+          // Description
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+            child: Text('Lore ipsum'),
+          )
+        ]
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Row(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Palacio de Beauclair',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Beuclair, Toussaint',
+                style: TextStyle(color: Colors.black45),
+              )
+            ],
+          ),
+          Expanded(child: Container()),
+          Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
+          Text('41'),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          CustomButton(icon: Icons.call, text: 'CALL'),
+          CustomButton(icon: Icons.near_me, text: 'ROUTE'),
+          CustomButton(icon: Icons.share, text: 'SHARE'),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const CustomButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          this.icon,
+          color: Colors.blue,
+          size: 30,
+        ),
+        Text(
+          this.text,
+          style: TextStyle(color: Colors.blue),
+        ),
+      ],
+    );
+  }
+}
