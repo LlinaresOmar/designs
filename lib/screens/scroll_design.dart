0c384957fe62b79cb12adc64b42c8827585f4f74
Omar Llinares <150013139+LlinaresOmar@users.noreply.github.com>
2024-10-13 17:52:04 +0200

    ScrollDesing acabado

--- lib/screens/scroll_design.dart
+++ lib/screens/scroll_design.dart
@@ -3,11 +3,32 @@
 import 'package:flutter/material.dart';
 
 class ScrollDesign extends StatelessWidget {
+  final boxDecoration = BoxDecoration(
+    gradient: LinearGradient(
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+      stops: [
+        0.5,
+        0.5
+      ],
+      colors: [
+        Color(0xff7EEBCD),
+        Color(0xff30BAD6)
+      ]));
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(children: [Page1(), Page1()])
+      body: Container(
+        decoration: boxDecoration,
+        child: PageView(
+          scrollDirection: Axis.vertical,
+          physics: BouncingScrollPhysics(),
+          children: [Page1(), Page2()],
+        ),
+      ),
     );
   }
 }
@@ -22,42 +43,9 @@ class Page1 extends StatelessWidget {
     return Stack(
       children: [
         Background(),
+
         MainContent()
-    
       ],
-    );
-  }
-}
-
-class MainContent extends StatelessWidget {
-  const MainContent({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    var textStyle = TextStyle(
-      fontSize: 50,
-      fontWeight: FontWeight.bold,
-      color: Colors.white
-    );
-    return SafeArea(
-      bottom: false,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 10),
-          Text('20º', style: textStyle),
-          Text('Viernes', style: textStyle),
-          Expanded(child: Container()),
-          Icon(
-            Icons.keyboard_arrow_down,
-             size: 100,
-            color: Colors.white
-          )
-        ],
-      ),
     );
   }
 }
@@ -73,3 +61,64 @@ class Background extends StatelessWidget {
     );
   }
 }
+
+class MainContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var textStyle = TextStyle(
+      fontSize: 50,
+      fontWeight: FontWeight.bold,
+      color: Colors.white
+    );
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 50
+          ),
+          Text(
+            '11º',
+            style: textStyle
+          ),
+          Text(
+            'Viernes',
+             style: textStyle
+          ),
+          Expanded(child: Container()),
+          Icon(
+            Icons.keyboard_arrow_down,
+            size: 100,
+            color: Colors.white
+          )
+        ]),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color:  Color(0xff30BAD6),
+      child: Center(
+        child: TextButton(
+          onPressed: (){},
+          child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40),
+          child: Text(
+            'Bienvenido',
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
+        ),
+        style: TextButton.styleFrom(
+          backgroundColor: Color(0xff00A2FF), shape: StadiumBorder()),
+        )
+      ),
+    );
+  }
+}
+
+
